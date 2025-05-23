fffaeb75893a7f5c0d735d8a4de4156b630f9aa3
Julian Seitz <112495164+julSeitz@users.noreply.github.com>
2025-05-23 11:57:48 +0200

    added check for error code

--- create_infra.sh
+++ create_infra.sh
@@ -2,16 +2,21 @@
 
 # Variables
 
-# Variables for VPC
+## Variables for VPC
 vpc_cidr="10.0.0.0/25"
 
 # Creating Infrastructure
 
+## Creating VPC
 vpc_id=$(aws ec2 create-vpc \
 --cidr-block $vpc_cidr \
 --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value="MyVpc"}]' \
 --query 'Vpc.VpcId' \
 --output text)
 
-echo "Created VPC"
-echo "VPC ID $vpc_id"
+if [[ $? -eq 0 ]]; then
+	echo "Created VPC"
+else
+	echo "An error occured while creating the VPC"
+	exit 1
+fi
