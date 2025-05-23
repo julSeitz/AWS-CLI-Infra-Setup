7bd1b5175a6f1df931ab681f6c84fdc497c6bd9d
Julian Seitz <112495164+julSeitz@users.noreply.github.com>
2025-05-23 12:30:09 +0200

    added Private Subnet creation

--- create_infra.sh
+++ create_infra.sh
@@ -7,6 +7,7 @@ vpc_cidr="10.0.0.0/25"
 
 ## Variables for creating Subnets
 pub_subnet_cidr="10.0.0.0/28"
+priv_subnet_cidr="10.0.0.64/26"
 
 # Creating Infrastructure
 
@@ -38,5 +39,20 @@ if [[ $? -eq 0 ]]; then
 	echo "Created Public Subnet"
 else
 	echo "An error occured while creating Public Subnet"
+	exit 1
+fi
+
+### Creating Private Subnet
+priv_subnet_id=$(aws ec2 create-subnet \
+--vpc-id $vpc_id \
+--cidr-block $priv_subnet_cidr \
+--tag-specifications 'ResourceType=subnet,Tags=[{Key=Name,Value="MyPrivateSubnet"}]' \
+--query 'Subnet.SubnetId' \
+--output text)
+
+if [[ $? -eq 0 ]]; then
+	echo "Created Private Subnet"
+else
+	echo "An error occured while creating Private Subnet"
 	exit 1
 fi
