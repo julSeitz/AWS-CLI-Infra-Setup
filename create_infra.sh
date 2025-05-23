c326b560cbba8ab4e0bf7aa3de9f92562a12789c
Julian Seitz <112495164+julSeitz@users.noreply.github.com>
2025-05-23 12:06:09 +0200

    added Public Subnet creation

--- create_infra.sh
+++ create_infra.sh
@@ -5,6 +5,9 @@
 ## Variables for VPC
 vpc_cidr="10.0.0.0/25"
 
+## Variables for creating Subnets
+pub_subnet_cidr="10.0.0.0/28"
+
 # Creating Infrastructure
 
 ## Creating VPC
@@ -18,5 +21,22 @@ if [[ $? -eq 0 ]]; then
 	echo "Created VPC"
 else
 	echo "An error occured while creating the VPC"
+	exit 1
+fi
+
+## Creating Subnets
+
+### Creating Public Subnet
+pub_subnet_id=$(aws ec2 create-subnet \
+--vpc-id $vpc_id \
+--cidr-block $pub_subnet_cidr \
+--tag-specifications 'ResourceType=subnet,Tags=[{Key=Name,Value="MyPublicSubnet"}]' \
+--query 'Subnet.SubnetId' \
+--output text)
+
+if [[ $? -eq 0 ]]; then
+	echo "Created Public Subnet"
+else
+	echo "An error occured while creating Public Subnet"
 	exit 1
 fi
